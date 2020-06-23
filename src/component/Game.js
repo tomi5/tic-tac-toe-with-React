5de27d2355dc65741fe263ab492cf2902ef6fd1f
Tomi5 <tomasz.rysak@o2.pl>
2020-06-23 13:21:54 +0200

    add start game button

--- src/component/Game.js
+++ src/component/Game.js
@@ -3,6 +3,12 @@ import Board from "./Board";
 
 import { calculateWinner } from "../helpers";
 
+const style = {
+  width: "400px",
+  margin: "20px auto",
+  fontSize: "30px",
+};
+
 const Game = () => {
   const [board, setBoard] = useState(Array(9).fill(null)); // start with empty board
   const [xIsNext, setXisNext] = useState(true);
@@ -20,9 +26,21 @@ const Game = () => {
 
   const jumpTo = () => {};
 
-  const renderMoves = () => {};
+  const renderMoves = () => (
+    <button onClick={() => setBoard(Array(9).fill(null))}>Start Game</button>
+  );
 
-  return <Board squares={board} onClick={handleClick} />;
+  return (
+    <>
+      <Board squares={board} onClick={handleClick} />
+      <div style={style}>
+        <p>
+          {winner ? `Winner: ${winner}` : `Next Player: ${xIsNext ? `X` : `O`}`}
+        </p>
+        {renderMoves()}
+      </div>
+    </>
+  );
 };
 
 export default Game;
