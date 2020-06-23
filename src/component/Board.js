7b63000785a768fd060627887e31cc1dc2f517b6
Tomi5 <tomasz.rysak@o2.pl>
2020-06-23 12:39:31 +0200

    create main components

---
+++ src/component/Board.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Square from "./Square";
+
+const Board = ({ squares, onclick }) => (
+  <div>
+    <Square value='1' onClick={() => onClick("dummy value")} />
+    <Square value='2' onClick={() => onClick("dummy value")} />
+    <Square value='3' onClick={() => onClick("dummy value")} />
+    <Square value='4' onClick={() => onClick("dummy value")} />
+    <Square value='5' onClick={() => onClick("dummy value")} />
+    <Square value='6' onClick={() => onClick("dummy value")} />
+    <Square value='7' onClick={() => onClick("dummy value")} />
+    <Square value='8' onClick={() => onClick("dummy value")} />
+    <Square value='9' onClick={() => onClick("dummy value")} />
+  </div>
+);
+
+export default Board;
---
+++ src/component/Game.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Board from "./Board";
+
+const Game = () => {
+  const handleClick = () => {};
+
+  const jumpTo = () => {};
+
+  const renderMoves = () => {};
+
+  return <Board onClick={handleClick} />;
+};
+
+export default Game;
