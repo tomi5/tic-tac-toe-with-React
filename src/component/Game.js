5b1299daafca6171b8effad2b883d6c8160038a5
Tomi5 <tomasz.rysak@o2.pl>
2020-06-23 13:07:29 +0200

    add hooks state

--- src/component/Game.js
+++ src/component/Game.js
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import Board from "./Board";
 
+import { calculateWinner } from "../helpers";
+
 const Game = () => {
+  const [board, setBoard] = useState(Array(9).fill(null)); // start with empty board
+  const [xIsNext, setXisNext] = useState(true);
+  const winner = calculateWinner(board);
+
   const handleClick = () => {};
 
   const jumpTo = () => {};
 
   const renderMoves = () => {};
 
-  return <Board onClick={handleClick} />;
+  return <Board squares={board} onClick={handleClick} />;
 };
 
 export default Game;
--- src/component/Square.js
+++ src/component/Square.js
@@ -1,7 +1,18 @@
 import React from "react";
 
+const style = {
+  background: "#dae1e7",
+  border: "2px solid #00909e",
+  fontSize: "30px",
+  fontWeight: "800",
+  cursor: "pointer",
+  outline: "none",
+};
+
 const Square = ({ value, onClick }) => (
-  <button onClick={onClick}>{value}</button>
+  <button style={style} onClick={onClick}>
+    {value}
+  </button>
 );
 
 export default Square;
